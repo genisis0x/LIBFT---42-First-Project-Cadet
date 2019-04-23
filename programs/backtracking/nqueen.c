2522079545ff4d3106b1b34b340fd4b1710e87a0
Manmeet <manietdavv@gmail.com>
2019-04-22 20:27:26 -0700

    Added delete.c

--- programs/backtracking/nqueen.c
+++ programs/backtracking/nqueen.c
@@ -6,69 +6,77 @@
 /*   By: maparmar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/09 20:55:09 by maparmar          #+#    #+#             */
-/*   Updated: 2019/03/09 23:03:54 by maparmar         ###   ########.fr       */
+/*   Updated: 2019/03/15 19:06:04 by maparmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include<stdbool.h>
 #define N 8
+static int k = 1;
 
 void printsolution(int board[N][N])
 {
+	k++;
+	//printf("The %d solution is\n", k++);
 	for (int i = 0; i < N; i++)
 	{
 		for(int j = 0; j < N; j++)
 		{
+			
 			if (board[i][j])
-				printf("Q ");
+				printf("%d", j + 1);
 			else
-				printf("- ");
+				;
+			;
 		}
-		printf("\n");
 	}
+	printf ("\n");
 }
 
-static int safe_way(int board[N][N], int row, int col)
+static bool safe_way(int board[N][N], int row, int col)
 {
 	int i = 0;
 	int j = 0;
 	while (i < col)
 	{
-		 if (board[row][i])
-			 return 0;
+		 if (board[row][i]) // check this row on left side
+			 return false;
 		 i++;
 	}
-	for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+	for (i = row, j = col; i >= 0 && j >= 0; i--, j--) // check the upper diagonal on left side
 	{
 		if (board[i][j])
-			return 0;
+			return false;
 	}
-	for (i = row, j = col; j >= 0 && i < N; i++, j--)
+	for (i = row, j = col; j >= 0 && i < N; i++, j--) // check the lower diagonal on left side
 	{
 		if (board[i][j])
-			return 0;
+			return false;
 	}
-	return 1;
+	return true;
 }
-
-
-static int ft_board_solver(int board[N][N], int col) // board solver fuction 
+static bool ft_board_solver(int board[N][N], int col) // board solver fuction 
 {
+	
 	if (col >= N)
-		return 1;
+	{
+		printsolution(board);
+		return true;
+	}
+	bool res = false;
 	for (int i = 0; i < N; i++) // integer i is used to iterate the each row 
 	{
 		if (safe_way(board, i, col)) // check if the position is safe or not
 		{
 			board[i][col] = 1;
-			if (ft_board_solver(board, col + 1)) // call recursively
-				return (1);
-			board[i][col] = 0;
+			if (ft_board_solver(board, col + 1))
+					res = true;// call recursively
+			board[i][col] = 0; // backtrack
 		}
 	}
-	return (0);
+	return (res);
 }
 
 
@@ -77,7 +85,5 @@ int main (void)
 	int board[N][N] = {{0}};
 	if(!ft_board_solver(board, 0)) // board and the coloum where we have to start 
 		printf("The Board has no solution");
-	else
-		printsolution(board);
 	return (0);
 }
